9e87e6649b7070fb89c9dfc53d3c3be132751dac
seojin kim <001106ksj@gmail.com>
2022-01-04 23:21:28 +0900

    feat: 첫 페이지 띄우기

--- kakao-book-api/src/pages/Home/index.tsx
+++ kakao-book-api/src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuthentication } from '../../hooks/useAuthentication';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
-  return <div></div>;
+  const { setToken, resetToken, isAuthenticated } = useAuthentication();
+  const navigate = useNavigate();
+  return isAuthenticated ? (
+    <>
+      <div>토큰을 이미 입력하셨어요!</div>
+      <div onClick={resetToken}>토큰 초기화하기</div>
+      <Link to='/search'>검색하러 가기</Link>
+    </>
+  ) : (
+    <>
+      <div>토큰을 안 입력하셨어요!</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formTarget = e.target as HTMLFormElement;
+          const inputTarget = formTarget.elements[0] as HTMLInputElement;
+          setToken(inputTarget.value);
+          navigate('/search');
+        }}
+      >
+        <input type='text' name='token_input' placeholder='토큰 입력하기' />
+      </form>
+    </>
+  );
 }
 
 export default HomePage;
